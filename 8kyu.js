d7f48be9d906671fadb50ff19c404ece487521ef
Vladimir Stasiuk <sv2109@gmail.com>
2022-01-23 12:27:19 +0200

    Update 8kyu.js

--- 8kyu.js
+++ 8kyu.js
@@ -1,6 +1,6 @@
 /**
  * Expressions Matter
- * {@link https://www.codewars.com/kata/5ae62fcf252e66d44d00008e}
+ * https://www.codewars.com/kata/5ae62fcf252e66d44d00008e
  * 
  * Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
  * In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
