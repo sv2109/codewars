8fbc07559866ef4878af10db8fa83b0d6cba5ecc
sv2109 <sv2109@gmail.com>
2022-01-23 12:22:02 +0200

    initial comment

---
+++ 8kyu.js
@@ -0,0 +1,18 @@
+/**
+ * Expressions Matter
+ * @link https://www.codewars.com/kata/5ae62fcf252e66d44d00008e
+ * 
+ * Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+ * In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+ * 
+ */
+ function expressionMatter(a, b, c) {
+  return Math.max(
+    a+b+c,
+    a*b*c,    
+    a*(b+c), 
+    (a+b)*c,
+    a+b*c,
+    a*b+c
+  )
+}
